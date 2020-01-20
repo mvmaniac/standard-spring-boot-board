7bbf444c72de60693f1074652c37bc3624fa4e15
yhs <mvmaniaz@gmail.com>
2020-01-20 23:05:13 +0900

    [추가] 초기 프로젝트 모듈 셋팅

---
+++ server-rest/src/main/java/io/devfactory/ServerRestApplication.java
@@ -0,0 +1,13 @@
+package io.devfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerRestApplication.class, args);
+    }
+
+}
