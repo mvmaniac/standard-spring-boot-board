92e185973719254cfa2254f830732c5ff9a377ad
yhs <mvmaniaz@gmail.com>
2020-04-27 21:44:07 +0900

    Refactor(web, rest): source cleanup

--- server-web/src/test/java/io/devfactory/ServerWebApplicationTest.java
+++ server-web/src/test/java/io/devfactory/ServerWebApplicationTest.java
@@ -3,7 +3,7 @@ package io.devfactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SuppressWarnings("squid:S1186")
+@SuppressWarnings("squid:S2699")
 @SpringBootTest
 class ServerWebApplicationTest {
 
